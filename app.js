03f6f2acf69266bb2d40062c87addf2ee2c9b271
Ralph Arsy Bauto <rab.devph@gmail.com>
2024-02-17 12:44:53 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 const express = require('express');
-const dotenv = require('dotenv');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const colors = require('colors');
 
-dotenv.config();
+const landlordRoutes = require('./routes/landlordRoutes');
+
+const { logErrors, errorHandler } = require('./middleware/errorMiddleware');
+
+const { initializeTables } = require('./scripts/createTables');
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`App is running on port ${colors.yellow.bold(port)}..`);
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+  console.log(`request path: ${req.path.cyan}`);
+  console.log(`request method: ${req.method.cyan}`);
+  next();
 });
+
+app.use('/api/landlords', landlordRoutes);
+
+app.use(logErrors);
+app.use(errorHandler);
+
+initializeTables()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(colors.green('Server is running on port', port));
+    });
+  })
+  .catch((err) => {
+    console.error(colors.red('Error:', err));
+    process.exit(-1);
+  });
